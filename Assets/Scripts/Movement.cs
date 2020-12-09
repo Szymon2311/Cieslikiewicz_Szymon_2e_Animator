a274fa0a1e4c8d70f3fb10a8fe525aa4212a5880
Szymon.C <szymekx@onet.pl>
2020-12-09 20:54:50 +0100

    added-project

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+
+        }
+    }
+
+    public void Jump()
+    {
+        animator.SetTrigger("Jump");
+    }
+}
